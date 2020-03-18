e193dd2d3db2762d036d820aac0d93371575338a
Irvin Chan <irvin@yellowme.mx>
2020-03-17 21:22:22 -0600

    Add before action authenticable

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,6 +1,8 @@
 module API
     module V1
         class GoalsController < APIController
+            before_action :authenticate_user
+
             def index
                 @goals = Goal.all
                 render json: @goals, each_serializer: GoalSerializer
