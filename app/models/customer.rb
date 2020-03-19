9869e779b677b0d841d05824fd5e5b0dc7d61b23
Irvin Chan <irvin@yellowme.mx>
2020-03-19 14:05:35 -0600

    Add ref between user and customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+    belongs_to :user
     has_and_belongs_to_many :foods
     has_and_belongs_to_many :goals
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_183510) do
+ActiveRecord::Schema.define(version: 2020_03_19_194930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_03_19_183510) do
     t.boolean "is_completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_customers_on_user_id"
   end
 
   create_table "customers_foods", id: false, force: :cascade do |t|
@@ -79,5 +81,6 @@ ActiveRecord::Schema.define(version: 2020_03_19_183510) do
     t.integer "role", default: 0
   end
 
+  add_foreign_key "customers", "users"
   add_foreign_key "foods", "food_categories"
 end
