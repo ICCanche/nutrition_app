0d432a90c1a0ab8054459a691e1f361bb57d9d4f
Irvin Chan <irvin@yellowme.mx>
2020-04-01 15:27:25 -0600

    Add pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   helper TimeZoneConverter
   
   def index
-    @diets = Diet.order(created_at: :desc)
+    @diets = Diet.order(created_at: :desc).page params[:page]
   end
 end
