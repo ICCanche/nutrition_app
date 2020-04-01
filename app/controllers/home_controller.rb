b1dbb2787c5458cd8b91d0e5256be1e36ccb9213
Irvin Chan <irvin@yellowme.mx>
2020-04-01 17:20:09 -0600

    Add filter action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   helper TimeZoneConverter
   
   def index
-    @diets = Diet.order(created_at: :desc).page params[:page]
+    if (!params.has_key?(:status) or params[:status].eql?"all")
+      @diets = Diet.order(created_at: :desc).page params[:page]
+    else
+      @diets = Diet.where(status: params[:status]).order(created_at: :desc).page params[:page]
+    end
   end
 end
