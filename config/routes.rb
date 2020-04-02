77f39f6ceef8ce10aea75879363cff40a35f4be4
Irvin Chan <irvin@yellowme.mx>
2020-04-02 10:49:50 -0600

    Add details

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
   post 'user_token' => 'user_token#create'
   resources :foods
+  resources :diets, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # ==================================
   # API subdomain
